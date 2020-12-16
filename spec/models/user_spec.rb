b601801812d7ffc66a3697332c4b738a6e2b3ce6
rukaeto <teraer0711@gmail.com>
2020-12-17 02:22:18 +0900

    tutorial 6 finish

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+RSpec.describe User, type: :model do
+  it "isn't less than 1 character" do
+    user = User.new
+    expect(user).not_to be_valid
+  end
+end
+
+
+
+
