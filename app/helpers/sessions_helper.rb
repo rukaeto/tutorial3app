4a1ce931394ef8fa65e2787f0c6212a39d59b6df
rukaeto <teraer0711@gmail.com>
2020-12-19 16:26:24 +0900

    9章の途中　次が9.1.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,40 @@
 module SessionsHelper
 
-  def log_in(user)
+  def log_in(user) #渡されたユーザーでログインする
     session[:user_id] = user.id
   end
 
-  def current_user
-    if session[:user_id]
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token #remember_tokenの期限を20年にして設定
+  end
+
+  def current_user #記憶トークンcookieに対応するユーザーを返す
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id:session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id:user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+
     end
   end
 
-  def logged_in?
+  def logged_in? #ユーザーがログインしていればtrue,それ以外ならfalseを返す
     !current_user.nil?
   end
 
-  def log_out
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out #現在のユーザーをログアウトする
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name,presence: true,
             length: {maximum: 50}
@@ -10,9 +12,26 @@ class User < ApplicationRecord
         has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
 
-  def User.digest(string)
+  def User.digest(string) #文字列のハッシュを返している
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token #ランダムなトークンを返す
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember #永続的なセッションのためにユーザーをデータベースに記憶する
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token) #渡されたトークンがダイジェストと一致したらtrueを返す
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget ## ユーザーのログイン情報を破棄する
+    update_attribute(:remember_digest,nil)
   end
 end
